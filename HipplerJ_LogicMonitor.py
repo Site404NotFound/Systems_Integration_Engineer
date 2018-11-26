fdc6239a170438d6077787339a6f325866abb5b6
Site404NotFound <hipplerj@gmail.com>
2018-11-26 11:46:31 -0600

    Adding python template before beginning technical interview

---
+++ HipplerJ_LogicMonitor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+"""
+Author: James Hippler (HipplerJ)
+Organization: LogicMonitor, Inc.
+Title: Systems Integration Engineer
+
+Description: Systems Integration Engineer Interview
+Completed: Monday, November 26, 2018
+
+Filename: HipplerJ_LogicMonitor.py
+*********************************************************************/
+"""
